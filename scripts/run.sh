a11d2ab8105582fcba08fe9ba2448cc1f33b1a8b
Sayantan Biswas <sayantan.biswas@epfl.ch>
2025-05-28 18:53:46 +0200

    grid search run setup

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,4 +13,11 @@ source /work/cvlab/students/bhagavan/ACI/.aci/bin/activate
 echo "Python binary: $(which python)"
 echo "Python version: $(python --version)"
 
-MUJOCO_GL=egl python3 cambrian/main.py --train example=detection
+for i in {1..3}; do
+    lot_ranges=(10 30 50 70 90 110 130 150 170)
+    for lot_range in "${lot_ranges[@]}"; do
+        low=$((-lot_range))
+        high=$((lot_range))
+        MUJOCO_GL=egl python3 cambrian/main.py --train example=detection env.agents.agent_prey.eyes.eye.lon_range="[$low,$high]"
+    done
+done
